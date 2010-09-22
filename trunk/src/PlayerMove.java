c4580100420fa118b1b9653bf7dc6301bfdf6773
aiqueneldar <aiqueneldar@074652e8-15f5-415c-45ea-77a17ba9196d>
2010-09-22 15:22:57 +0000

    A version of Player move that compiles. But it's not tested for logical errors at all.

--- trunk/src/PlayerMove.java
+++ trunk/src/PlayerMove.java
@@ -1,4 +1,6 @@
+import java.util.LinkedList;
 import java.util.List;
+import java.rmi.NotBoundException;
 
 /**
  * A class to move the player around to puch the boxes
@@ -8,7 +10,7 @@ import java.util.List;
 public class PlayerMove {
 	private Board startBoard;
 	private List<Move> moveSequence;
-	private List<Vortex>[][] graph;
+	private Vortex[][] graph;
 	private List<Vortex> nodequeue;
 
 	// Initialize the class by setting variables, then start the search for the path to take
@@ -32,12 +34,12 @@ public class PlayerMove {
 
 	public void createVortexes() {
 		this.nodequeue = new LinkedList<Vortex>();
-		for (int i = 0; i < this.startBoard.floor.lenght; i++) {
+		for (int i = 0; i < this.startBoard.floor.length; i++) {
 			for (int j = 0; j < this.startBoard.floor[0].length; j++) {
 				// If it is a reachable tile, add a vortex point there.
 				// This will also automatically give us edges
 				if (this.startBoard.reachable[i][j]) {
-					this.graph[i][j] = new Vortex(i, j, Integer.MAX_INT, null, false);
+					this.graph[i][j] = new Vortex(i, j, Integer.MAX_VALUE, null, false);
 					this.nodequeue.add(this.graph[i][j]);
 				}
 			}
@@ -66,33 +68,52 @@ public class PlayerMove {
 		String steps = "";
 		while (this.nodequeue.size() > 0) {
 			Vortex u = this.leastDist();
-			if (u.dist == Integer.MAX_INT) {
+			if (u.dist == Integer.MAX_VALUE) {
 				throw new NotBoundException("No awailable path to target tile!");
 			}
 			if (u.x == box.x && u.y == box.y) {
 				steps = this.getPath(u);
-				return steps;
+				break;
 			}
-			if (u.x + 1 < this.graph.size())
-				this.updateDist(this.graph[u.x + 1][u.y]);
+			if (u.x + 1 < this.graph.length)
+				this.updateDist(this.graph[u.x + 1][u.y], u);
 			
 			if (u.x - 1 > 0)
-				this.updateDist(this.graph[u.x - 1][u.y]);
+				this.updateDist(this.graph[u.x - 1][u.y], u);
 			
-			if (u.y + 1 < this.graph[u.x].size())
-				this.updateDist(this.graph[u.x][u.y + 1]);
+			if (u.y + 1 < this.graph[u.x].length)
+				this.updateDist(this.graph[u.x][u.y + 1], u);
 
 			if (u.y - 1 > 0)
-				this.updateDist(this.graph[u.x].size());
+				this.updateDist(this.graph[u.x][u.y - 1], u);
 		}
+		return steps;
 	}
 			
 	public Vortex leastDist() {
-	
+		int dist = Integer.MAX_VALUE;
+		Vortex returnNode = null;
+		for (Vortex node : this.nodequeue) {
+			if (node.dist < dist) {
+				dist = node.dist;
+				returnNode = node;
+			}
+		}
+
+		return returnNode;
 	}
 
 	public String getPath(Vortex node) {
+		return "";
 
 	}
 	
+	public void updateDist(Vortex node, Vortex parent) {
+		int alt = node.dist + 1;
+		if (alt < node.dist) {
+			node.dist++;
+			node.parent = parent;
+		}
+
+	}
 }
