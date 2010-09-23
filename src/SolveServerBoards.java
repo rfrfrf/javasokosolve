ccccef16b6c6462575ab4e9fc3d56dea8ea88fa1
janspambox <janspambox@gmx.de>
2010-09-23 11:12:06 +0000

    Server board testing class added, minor fixes/enhancements

---
+++ src/SolveServerBoards.java
@@ -0,0 +1,57 @@
+import java.io.IOException;
+import java.util.Date;
+import java.util.List;
+
+
+public class SolveServerBoards {
+	
+	private static final int TIMEOUT = 30000;
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws IOException, InterruptedException {		
+		for (int i = 1; i<1000; i++) {
+			testBoard(i);
+			System.gc();
+		}
+	}
+	
+	private static void testBoard(int number) throws IOException, InterruptedException {
+		Board b = BoardLoader.loadFromServer(number);
+		System.out.format("Solving board number %3d...", number);
+				
+		SolverThread thread = new SolverThread(b);
+
+		long startTime = (new Date()).getTime();
+		thread.start();
+
+		while (!thread.isFinished) {
+			synchronized (thread.waitingObject) {
+				thread.waitingObject.wait(1000);
+			}
+			double elapsedTime = (new Date()).getTime() - startTime;
+			if (elapsedTime > TIMEOUT) {
+				thread.stop();
+				System.out.format("TIMEOUT (%.2f seconds = %.2f minutes)\n", elapsedTime/1000, elapsedTime/60000);
+				return;				
+			}
+		}
+		double neededTime = (new Date()).getTime() - startTime;
+
+		List<Move> solution = thread.solution;
+		if (solution == null) {
+			System.out.format(" NO SOLUTION FOUND (%.2f seconds = %.2f minutes)\n", neededTime/1000, neededTime/60000);
+			return;
+		}
+		
+		String solutionString = ""; // TODO: Use PlayerMove here!
+
+		boolean result = BoardLoader.checkSolution(number, solutionString);
+		
+		System.out.print(result? "ok" : "WRONG");
+		
+		System.out.format(" (%.2f seconds = %.2f minutes)\n", neededTime/1000, neededTime/60000);
+	}
+
+}
