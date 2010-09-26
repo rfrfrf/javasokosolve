f3075a00ecb6fe385e1c58506a5cc88aaf45be87
janspambox <janspambox@gmx.de>
2010-09-26 00:36:40 +0000

    minor fixes to Djikstra in PlayerMove

--- src/PlayerMove.java
+++ src/PlayerMove.java
@@ -17,18 +17,6 @@ public class PlayerMove {
 	public PlayerMove(Board startBoard, List<Move> moveSequence) {
 		this.startBoard = startBoard;
 		this.moveSequence = moveSequence;
-		
-		startBoard.calculateMaps(); // make sure maps are calculated! 
-
-		// To use Dijkstras we first build a matrix for the graph
-		this.graph = new Vortex[this.startBoard.floor.length][this.startBoard.floor[0].length];
-		//Create all vortexes in the matrix
-		this.createVortexes();
-		// Set the startingpositions special values
-		this.graph[0][0].isRoot = true;
-		this.graph[0][0].dist = 0;
-		
-		
 	}
 
 	public void createVortexes() {
@@ -48,7 +36,6 @@ public class PlayerMove {
 
 	public String findPath() {
 		String movements = "";
-		this.startBoard.calculateMaps();
 		List<Move> dir = this.moveSequence;
 		for (Move curr : dir) {
 			try {
@@ -66,7 +53,17 @@ public class PlayerMove {
 	// in the name to be understood as the "There is no path to your goal"exception. -- aiquen
 	public String localPath(Move box) throws NotBoundException {
 		String steps = "";
+		
+		// To use Dijkstras we first build a matrix for the graph
+		this.graph = new Vortex[this.startBoard.floor.length][this.startBoard.floor[0].length];
+		//Create all vortexes in the matrix
+		startBoard.calculateMaps();
 		this.createVortexes();
+		// Set the starting positions special values
+		this.graph[startBoard.player.x][startBoard.player.y].isRoot = true;
+		this.graph[startBoard.player.x][startBoard.player.y].dist = 0;
+		
+		
 		int dir = box.direction;
 		int dx = 0;
 		int dy = 0;
@@ -80,7 +77,7 @@ public class PlayerMove {
 		while (this.nodequeue.size() > 0) {
 			Vortex u = this.leastDist();
 			if (u.dist == Integer.MAX_VALUE) {
-				throw new NotBoundException("No awailable path to target tile!");
+				throw new NotBoundException("No available path to target tile!");
 			}
 			if (u.x == box.box.x + dx && u.y == box.box.y + dy) {
 				steps = this.getPath(u);
@@ -111,7 +108,7 @@ public class PlayerMove {
 				returnNode = node;
 			}
 		}
-
+		nodequeue.remove(returnNode);
 		return returnNode;
 	}
 	//TODO - Everything in this method. Sunborg is in charge -- aiquen
@@ -121,6 +118,7 @@ public class PlayerMove {
 	}
 	
 	public void updateDist(Vortex node, Vortex parent) {
+		if (node == null) return; // nothing to do here
 		int alt = node.dist + 1;
 		if (alt < node.dist) {
 			node.dist++;
